a8ce3934f99d3e45aedd467000959c0657e76758
WenBo <wenbo.zhao.1998@gmail.com>
2023-10-01 17:02:18 +0100

    Add coordinates to the board

--- board.py
+++ board.py
@@ -19,8 +19,41 @@ class Board:
         #     print(self._board[row])
 
         board_str = ""
+        row_counter = len(self._board) - 2
+        last_row = False
         for row in self._board:
+            if not last_row:
+                board_str += str(row_counter) + " "
+            else:
+                board_str += "  "
+                
             row_str = " ".join(map(str, row))
             board_str += f"{row_str}\n"
+            
+            if row_counter >= 0:
+                row_counter -= 1
+            
+            if row_counter < 0:
+                last_row = True
         
         return board_str
+
+    # Functions that determine game logic
+
+    # This function starts the game
+    def run_game(self):
+        while True:
+            user_coords = input("Please enter the coordinates of where you want to place your coin x,y: ")
+            self.valid_move(user_coords)
+            break;
+
+    def valid_move(self, coordinates: str) -> bool:
+        # coordinate cannot be out of bounds
+        # there has to be a coin below it unless it's in the last row
+
+        # first we need to break the coordinate into x and y coords
+        x_coord = coordinates.split(",")[0]
+        y_coord = coordinates.split(",")[1]
+
+        # now we need some logic to validate the coordinates
+
--- main.py
+++ main.py
@@ -2,3 +2,5 @@ from board import Board
 board = Board()
 
 print(board)
+
+board.run_game()
